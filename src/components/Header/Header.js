1c421328cba39fd968315531fd40716ef81f88ea
David Isidore <isidoredavid@protonmail.com>
2023-03-06 17:23:11 +0100

    Mise en place de la barre de navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 //import logo from '../../logo.svg'
 import photo from '../../assets/Photo2.png'
 import './header.css'
+import Navbar from './Navbar/Navbar'
 
 export default function Header() {
     return (
@@ -10,6 +11,7 @@ export default function Header() {
             <div className="bandeau">
                 <h1>David ISIDORE</h1>
                 <h2>Développeur Front-End Junior</h2>
+                <Navbar />
             </div>
         </div>
     )
--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
-    return <div></div>
+    return (
+        <nav className="nav">
+            <ul className="nav_list">
+                <li className="accueil">
+                    <Link to="/">Accueil</Link>
+                </li>
+                <li className="contact">
+                    <Link to="/">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
