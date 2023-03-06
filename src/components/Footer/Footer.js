b530bf2d254525fb2e750a28dd6dd5530665589d
David Isidore <isidoredavid@protonmail.com>
2023-03-06 17:59:18 +0100

    Implémentation de la structure du footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,18 @@
 import React from 'react'
+import logoGit from '../../assets/github.svg'
+import logo_linked from '../../assets/linkedin.svg'
+import logo_arch from '../../assets/archlinux.svg'
 export default function Footer() {
-    return <div></div>
+    return (
+        <div>
+            <div className="logos">
+                <img src={logoGit} alt="Logo Github" />
+                <img src={logo_linked} alt="Logo LinkedIn" />
+            </div>
+            <div className="stack_dev">
+                <p>Développé sous :</p>
+                <img src={logo_arch} alt="Logo Archlinux" />
+            </div>
+        </div>
+    )
 }
