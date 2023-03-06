a0b81ea698426a232c80a611fa4224f85d00838b
David Isidore <isidoredavid@protonmail.com>
2023-03-06 18:07:52 +0100

    Mise en place du style du footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,25 @@ import React from 'react'
 import logoGit from '../../assets/github.svg'
 import logo_linked from '../../assets/linkedin.svg'
 import logo_arch from '../../assets/archlinux.svg'
+import './footer.css'
 export default function Footer() {
     return (
-        <div>
+        <div className="footer">
             <div className="logos">
-                <img src={logoGit} alt="Logo Github" />
-                <img src={logo_linked} alt="Logo LinkedIn" />
+                <img src={logoGit} className="logoGit" alt="Logo Github" />
+                <img
+                    src={logo_linked}
+                    className="logoLinked"
+                    alt="Logo LinkedIn"
+                />
             </div>
             <div className="stack_dev">
                 <p>Développé sous :</p>
-                <img src={logo_arch} alt="Logo Archlinux" />
+                <img
+                    src={logo_arch}
+                    className="logoArch"
+                    alt="Logo Archlinux"
+                />
             </div>
         </div>
     )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,12 @@
-import React from "react";
-import Header from "../../components/Header/Header";
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 
 export default function Home() {
-    return(
-        <Header />
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
     )
 }
