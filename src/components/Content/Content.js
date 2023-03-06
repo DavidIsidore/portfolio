22e214340c45aadb6be0cef39b540a638f87a069
David Isidore <isidoredavid@protonmail.com>
2023-03-06 21:16:59 +0100

    Iplémentation des pages About, Myway et Myworks

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './content.css'
+import { Link } from 'react-router-dom'
 
 export default function Content() {
     return (
         <main className="content">
-            <div>Qui suis-je</div>
-            <div>Mes réalisations</div>
-            <div> </div>
+            <div className="content_about">
+                <Link to="/about">Qui suis-je</Link>
+            </div>
+            <div className="content_way">
+                <Link to="myway">Mon parcours</Link>
+            </div>
+            <div className="content_done">
+                <Link to="myworks">Mes réalisations</Link>
+            </div>
         </main>
     )
 }
