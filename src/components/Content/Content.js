dc347e7b304a5bc1f4d238cae4fd3bc0f4595b3a
David Isidore <isidoredavid@protonmail.com>
2023-03-06 20:52:35 +0100

    Implémentation + début de la mise en place du style pour le composant Content

---
+++ src/components/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './content.css'
+
+export default function Content() {
+    return (
+        <main className="content">
+            <div>Qui suis-je</div>
+            <div>Mes réalisations</div>
+            <div> </div>
+        </main>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import Content from '../../components/Content/Content'
 import { useEffect } from 'react'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     return (
         <div>
             <Header />
+            <Content />
             <Footer />
         </div>
     )
