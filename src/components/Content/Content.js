28296ca9292c77e635d54e761e153147fc1e8824
David Isidore <isidoredavid@protonmail.com>
2023-03-06 21:20:32 +0100

    Modification de la taille de police du composant Content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -6,13 +6,19 @@ export default function Content() {
     return (
         <main className="content">
             <div className="content_about">
-                <Link to="/about">Qui suis-je</Link>
+                <Link to="/about" className="quisuisje">
+                    Qui suis-je
+                </Link>
             </div>
             <div className="content_way">
-                <Link to="myway">Mon parcours</Link>
+                <Link to="myway" className="myway">
+                    Mon parcours
+                </Link>
             </div>
             <div className="content_done">
-                <Link to="myworks">Mes réalisations</Link>
+                <Link to="myworks" className="myworks">
+                    Mes réalisations
+                </Link>
             </div>
         </main>
     )
