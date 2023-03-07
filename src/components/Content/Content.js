3687c9fbef84a334bc4f23423e783242eb00ab72
David Isidore <isidoredavid@protonmail.com>
2023-03-07 08:25:38 +0100

    Modification de package.json

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom'
 export default function Content() {
     return (
         <main className="content">
-            <div className="content_about">
+            <div className="content_single">
                 <Link to="/about" className="quisuisje">
                     Qui suis-je
                 </Link>
             </div>
-            <div className="content_way">
+            <div className="content_single">
                 <Link to="myway" className="myway">
                     Mon parcours
                 </Link>
             </div>
-            <div className="content_done">
+            <div className="content_single">
                 <Link to="myworks" className="myworks">
                     Mes réalisations
                 </Link>
