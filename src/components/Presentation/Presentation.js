a87ef7aefe6ab0ee737112aa8a4082433a3426d3
David Isidore <isidoredavid@protonmail.com>
2023-03-23 15:43:20 +0100

    Modification de la mise en forme de la page About

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import computer from '../../assets/Computer.png'
 import photo from '../../assets/Photo.png'
+import './presentation.scss'
 
 export default function Presentation() {
     return (
-        <main>
-            <div className="content_text">
+        <main className="presentation">
+            <div className="presentation_text">
                 Hello! Vous êtes au bon endroit pour en savoir un peu plus sur
                 moi. Mais, pour commencer, qui voulez-vous connaître mieux en
                 premier?
             </div>
-            <div className="content_presentation">
-                <div className="content_presentation_img">
+            <div className="presentation_block">
+                <div className="presentation_block_img">
                     <img src={photo} alt="identité" />
+                    <p>La personne</p>
                 </div>
-                <div className="content_presentation_img ">
+                <div className="presentation_block_img">
                     <img
                         src={computer}
                         alt="représentation écran"
                         className="img_single"
                     />
+                    <p>Le développeur</p>
                 </div>
             </div>
         </main>
