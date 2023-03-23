331fdfecaf9b259894ef072dc68dfa0136e02618
David Isidore <isidoredavid@protonmail.com>
2023-03-23 15:17:52 +0100

    Modification des noms alternatifs des photos

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './content.scss'
 import computer from '../../assets/Computer.png'
+import photo from '../../assets/Photo.png'
 
 export default function Content() {
     return (
         <main className="content">
             <div className="content_text">Bla Bla</div>
             <div className="content_presentation">
-                <div></div>
-                <div>
-                    <img src={computer} alt="image écran" />
+                <div className="content_presentation_img">
+                    <img src={photo} alt="identité" />
+                </div>
+                <div className="content_presentation_img ">
+                    <img
+                        src={computer}
+                        alt="représentation écran"
+                        className="img_single"
+                    />
                 </div>
             </div>
         </main>
