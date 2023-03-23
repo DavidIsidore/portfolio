cdcabce4b5511bbd39e2cc4cda6bd18d8600010b
David Isidore <isidoredavid@protonmail.com>
2023-03-23 14:05:51 +0100

    Implémentation du Header

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 export default function Home() {
-    return <div>Page d'accueil</div>
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <h1>David ISIDORE</h1>
+                <h2>Développeur Front-End Junior</h2>
+            </div>
+        </div>
+    )
+}
