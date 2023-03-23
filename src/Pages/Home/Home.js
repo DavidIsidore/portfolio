ddb57aaaa7f612e58c3c5a4e95cd9e1514975dda
David Isidore <isidoredavid@protonmail.com>
2023-03-23 14:57:45 +0100

    Implémentation du composant Content et import d'image

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Content from '../../components/Content/Content'
 
 export default function Home() {
     return (
         <div>
             <Header />
+            <Content />
             <Footer />
         </div>
     )
---
+++ src/components/Content/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './content.scss'
+
+export default function Content() {
+    return (
+        <main className="content">
+            <div className="content_text">Bla Bla</div>
+            <div className="content_presentation">
+                <div></div>
+                <div></div>
+            </div>
+        </main>
+    )
+}
