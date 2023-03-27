dadcb2fcae83e6f017676c02cf93f3baa900e3f1
David Isidore <isidoredavid@protonmail.com>
2023-03-27 14:18:11 +0200

    Ajout du surlignement de la page active

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,69 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.scss'
-import home from '../../assets/house-solid.svg'
-import user from '../../assets/user-solid.svg'
-import code from '../../assets/code-solid.svg'
-import envelop from '../../assets/envelope-solid.svg'
+//import home from '../../assets/house-solid.svg'
+//import user from '../../assets/user-solid.svg'
+//import code from '../../assets/code-solid.svg'
+//import envelop from '../../assets/envelope-solid.svg'
+import { useLocation } from 'react-router-dom'
 
 export default function Navbar() {
+    const location = useLocation()
+    console.log(location)
+
     return (
         <nav className="nav">
             <ul className="nav_list">
                 <li className="accueil">
                     <Link to="/">
-                        <img src={home} alt="home" className="nav_list_logo" />
-                        <p className="nav_list_text">Accueil</p>
+                        <p
+                            className={
+                                location.pathname === '/'
+                                    ? 'nav_list_text_active'
+                                    : 'nav_ist_text'
+                            }
+                        >
+                            Accueil
+                        </p>
                     </Link>
                 </li>
                 <li className="about">
                     <Link to="/about">
-                        <img src={user} alt="user" className="nav_list_logo" />
-                        <p className="nav_list_text">Qui suis-je</p>
+                        <p
+                            className={
+                                location.pathname === '/about'
+                                    ? 'nav_list_text_active'
+                                    : 'nav_list-text'
+                            }
+                        >
+                            Qui suis-je
+                        </p>
                     </Link>
                 </li>
                 <li className="projets">
                     <Link to="/projets">
-                        <img src={code} alt="code" className="nav_list_logo" />
-                        <p className="nav_list_text">Mes projets</p>
+                        <p
+                            className={
+                                location.pathname === '/projets'
+                                    ? 'nav_list_text_active'
+                                    : 'nav_list_text'
+                            }
+                        >
+                            Mes projets
+                        </p>
                     </Link>
                 </li>
                 <li className="contact">
                     <Link to="/contact">
-                        <img
-                            src={envelop}
-                            alt="enveloppe"
-                            className="nav_list_logo"
-                        />{' '}
-                        <p className="nav_list_text">Contact</p>
+                        <p
+                            className={
+                                location.pathname === '/contact'
+                                    ? 'nav_list_text_active'
+                                    : 'nav_list_text'
+                            }
+                        >
+                            Contact
+                        </p>
                     </Link>
                 </li>
             </ul>
