6e15d09068d0f18aa626895b904f2a5a42f2e324
David Isidore <isidoredavid@protonmail.com>
2023-03-27 15:00:19 +0200

    Ajout du surlignement de la page active

---
+++ src/datas/data_projets.js
@@ -0,0 +1,36 @@
+const datas_projets = [
+    {
+        link: 'https://davidisidore.github.io/Livrables-P2/',
+        img: '../assets/Booki.png',
+        description:
+            'Premier projet développé, dans le cadre de ma reconversion, avec HTML et CSS',
+    },
+    {
+        link: 'https://davidisidore.github.io/Projet3/',
+        img: '../assets/ohmyfood.png',
+        description: 'Deuxième projet, développé en HTML et Sass',
+    },
+    {
+        link: [
+            'https://davidisidore.github.io/P4-initial/',
+            'https://davidisidore.github.io/LaPanthere/',
+        ],
+        img: '../assets/la_panthere.png',
+        description: "OPtimisation d'un site existant",
+    },
+    {
+        link: 'https://github.com/DavidIsidore/P5-Kanap',
+        img: '../assets/kanap.png',
+        description: "Développement Front-End de l'application",
+    },
+    {
+        link: 'https://github.com/DavidIsidore/Projet6-Piiquante',
+        img: '../assets/piiquante.png',
+        description: "Développement Back-End de l'application",
+    },
+    {
+        link: 'https://davidisidore.github.io/Kasa/',
+        img: '../assets/Kasa.svg',
+        description: "Développement de l'application Kasa avec React",
+    },
+]
