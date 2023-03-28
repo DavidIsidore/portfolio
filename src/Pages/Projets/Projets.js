6ee260aca35bc4d5210f57d257ae8b3c95162b25
David Isidore <isidoredavid@protonmail.com>
2023-03-28 14:08:40 +0200

    Suppression des data

--- src/Pages/Projets/Projets.js
+++ src/Pages/Projets/Projets.js
@@ -2,14 +2,13 @@ import React from 'react'
 import Bandeau from '../../components/Bandeau/Bandeau'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import Realisations from '../../components/Realisations/Realisations'
 
 export default function Projets() {
     return (
         <div>
             <Bandeau />
             <Header />
-            <Realisations />
+
             <Footer />
         </div>
     )
