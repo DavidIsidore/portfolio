828adbaaa5c4afe6ffc5d7d49f11f57598fc2fd5
David Isidore <isidoredavid@protonmail.com>
2023-03-28 15:29:03 +0200

    Ajout du bandeau supérieur sur la page Qui suis-je

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Bandeau from '../../components/Bandeau/Bandeau'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 
 export default function About() {
     return (
         <div>
+            <Bandeau />
             <Header />
             <Footer />
         </div>
