fd4716b02b2caf8ac254493ac56c12200976757c
David Isidore <isidoredavid@protonmail.com>
2023-03-28 16:36:26 +0200

    Ajout de l'icone LinkedIn

--- src/components/Callme/Callme.js
+++ src/components/Callme/Callme.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import phone from '../../assets/phone-solid.svg'
 import envelop from '../../assets/envelope-solid.svg'
+import linkedin from '../../assets/linkedin.svg'
 import './callme.scss'
 
 export default function Callme() {
     return (
-        <div>
+        <div className="join">
             <p>Vous pouvez me joindre</p>
-            <div className="joinme">
-                <div>
-                    <img src={phone} alt="telephone" className="joinme_logo" />
+            <div className="join_me">
+                <div className="call">
+                    <img src={phone} alt="telephone" className="join_me_logo" />
                     <p>06 79 85 25 16</p>
                 </div>
-                <div>
+                <div className="mail">
                     <img
                         src={envelop}
                         alt="enveloppe"
-                        className="joinme_logo"
+                        className="join_me_logo"
                     />
                     <p>isidoredavid@gmail.com</p>
+                </div>
+                <div className="linked">
+                    <img
+                        src={linkedin}
+                        alt="linkedin"
+                        className="join_me_logo"
+                    />
+                    <p>Sur LinkedIn</p>
                 </div>
             </div>
         </div>
