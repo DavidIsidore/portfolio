8ea3f0e52d2a633e17cda77ef0a4d0c3d201b4f0
David Isidore <isidoredavid@protonmail.com>
2023-03-30 08:34:22 +0200

    Ajout de texte de la partie about et mise en forme

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,12 +3,34 @@ import Bandeau from '../../components/Bandeau/Bandeau'
 import Button from '../../components/Button/Button'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import './about.scss'
 
 export default function About() {
     return (
         <div>
             <Bandeau />
             <Header />
+            <div className="about_text">
+                <p>
+                    Jeune talent en devenir, je me suis formé au développement
+                    Web sur la plateforme OpenClassrooms suite à une
+                    reconversion professionnelle.
+                </p>
+                <p>
+                    {' '}
+                    Passionné depuis de longues années par le milieu de
+                    l'informatique, j'ai décidé de m'orienter vers le
+                    Développement afin de participer à la mise en place du Web
+                    de demain.
+                </p>
+                <p>
+                    {' '}
+                    Rigoureux et curieux mais pas encore assez expérimenté,
+                    j'apporte toutefois avec moi de solides années d'expérience
+                    issues du monde de l'industrie et une envie d'apprendre qui
+                    n'a d'égale que celle de me perfectionner.
+                </p>
+            </div>
             <Button />
             <Footer />
         </div>
--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import computer from '../../assets/Computer.png'
 import photo from '../../assets/Photo.png'
 import './presentation.scss'
+import { Link } from 'react-router-dom'
 
 export default function Presentation() {
     return (
@@ -14,18 +15,28 @@ export default function Presentation() {
                 <p>Qui voulez-vous découvrir en premier? </p>
             </div>
             <div className="presentation_block">
-                <div className="presentation_block_img">
-                    <img src={photo} alt="identité" className="img_single" />
-                    <p className="presentation_block_text">La personne</p>
-                </div>
-                <div className="presentation_block_img">
-                    <img
-                        src={computer}
-                        alt="représentation écran"
-                        className="img_single"
-                    />
-                    <p className="presentation_block_text">Le développeur</p>
-                </div>
+                <Link to="/about">
+                    <div className="presentation_block_img">
+                        <img
+                            src={photo}
+                            alt="identité"
+                            className="img_single"
+                        />
+                        <p className="presentation_block_text">La personne</p>
+                    </div>
+                </Link>
+                <Link to="/projets">
+                    <div className="presentation_block_img">
+                        <img
+                            src={computer}
+                            alt="représentation écran"
+                            className="img_single"
+                        />
+                        <p className="presentation_block_text">
+                            Le développeur
+                        </p>
+                    </div>
+                </Link>
             </div>
         </main>
     )
