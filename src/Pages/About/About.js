9b2ec0fda345b0f83efb655125b92681fc070817
David Isidore <isidoredavid@protonmail.com>
2023-03-30 08:58:22 +0200

    de texte dans la partie about et mise en forme

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,6 +3,12 @@ import Bandeau from '../../components/Bandeau/Bandeau'
 import Button from '../../components/Button/Button'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import html from '../../assets/html5.svg'
+import css from '../../assets/css3-alt.svg'
+import js from '../../assets/square-js.svg'
+import react from '../../assets/react.svg'
+import figma from '../../assets/figma.svg'
+import wordpress from '../../assets/wordpress.svg'
 import './about.scss'
 
 export default function About() {
@@ -30,6 +36,31 @@ export default function About() {
                     issues du monde de l'industrie et une envie d'apprendre qui
                     n'a d'égale que celle de me perfectionner.
                 </p>
+            </div>
+            <div className="about_text">
+                <h2 className="about_text_title">
+                    Les outils avec lesquels j'ai travaillé
+                </h2>
+                <ul className="list">
+                    <li>
+                        <img src={html} alt="HTML" className="list_icon" />
+                    </li>
+                    <li>
+                        <img src={css} alt="HTML" className="list_icon" />
+                    </li>
+                    <li>
+                        <img src={js} alt="HTML" className="list_icon" />
+                    </li>
+                    <li>
+                        <img src={react} alt="HTML" className="list_icon" />
+                    </li>
+                    <li>
+                        <img src={figma} alt="HTML" className="list_icon" />
+                    </li>
+                    <li>
+                        <img src={wordpress} alt="HTML" className="list_icon" />
+                    </li>
+                </ul>
             </div>
             <Button />
             <Footer />
