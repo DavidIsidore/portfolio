3a494105a6b66891dc3d7c14da58f1d8c4634ed7
David Isidore <isidoredavid@protonmail.com>
2023-03-31 21:47:25 +0200

    Utilisation de useEffect pour renommer la page About

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Bandeau from '../../components/Bandeau/Bandeau'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 
 import Content from '../../components/Content/Content'
+import { useEffect } from 'react'
 
 export default function About() {
+    useEffect(() => {
+        document.title = 'À propos'
+    })
     return (
         <div>
             <Bandeau />
