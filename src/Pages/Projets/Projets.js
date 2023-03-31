6b2646d0502ce4c1b5e9fd31cf95c970094fdae7
David Isidore <isidoredavid@protonmail.com>
2023-03-31 21:48:39 +0200

    Utilisation de useEffect pour renommer la page Projets

--- src/Pages/Projets/Projets.js
+++ src/Pages/Projets/Projets.js
@@ -3,8 +3,12 @@ import Bandeau from '../../components/Bandeau/Bandeau'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Realisations from '../../components/Realisations/Realisations'
+import { useEffect } from 'react'
 
 export default function Projets() {
+    useEffect(() => {
+        document.title = 'Projets'
+    })
     return (
         <div>
             <Bandeau />
