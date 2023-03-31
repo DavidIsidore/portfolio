88783f5a3866837a5d0af602bf44a90c55988981
David Isidore <isidoredavid@protonmail.com>
2023-03-31 21:45:42 +0200

    Utilisation de useEffect pour renommer la page Contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,8 +4,12 @@ import Bandeau from '../../components/Bandeau/Bandeau'
 import Footer from '../../components/Footer/Footer'
 import './contact.scss'
 import Callme from '../../components/Callme/Callme'
+import { useEffect } from 'react'
 
 export default function Contact() {
+    useEffect(() => {
+        document.title = 'Contact'
+    })
     return (
         <main>
             <Bandeau />
