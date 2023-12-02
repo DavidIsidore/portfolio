5ea1dcabf08cb2b49fe168117971dedcdbf2c167
David Isidore <110615238+DavidIsidore@users.noreply.github.com>
2023-12-02 05:02:43 +0100

    Update Presentation.js

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import computer from '../../assets/Computer.png'
 //import photo from '../../assets/Photo.png'
-import photo_presentation from './../assets/Photo_présentation.jpg'
+import photo2 from './../assets/Photo2.jpg'
 import './presentation.scss'
 import { Link } from 'react-router-dom'
 
