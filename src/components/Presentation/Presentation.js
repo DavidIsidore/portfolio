54ebb558152c77c31b1893f9396feeb7010a0540
David Isidore <110615238+DavidIsidore@users.noreply.github.com>
2023-12-02 04:49:58 +0100

    Update Presentation.js
    Update de la photo de présentation

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import computer from '../../assets/Computer.png'
 import photo from '../../assets/Photo.png'
+import photo-presentation from './../assets/Photo présentation.jpg'
 import './presentation.scss'
 import { Link } from 'react-router-dom'
 
@@ -22,7 +23,7 @@ export default function Presentation() {
                 <Link to="/about" className="presentation_block_img">
                     <div>
                         <img
-                            src={photo}
+                            src={photo-presentation}
                             alt="identité"
                             className="presentation_block_img_single"
                         />
